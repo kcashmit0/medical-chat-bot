cd7d7259d9e163ecc137478378cb483137ddbc85
Ashmit K C <kcashmit0@gmail.com>
2026-01-02 22:13:16 +1030

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+setup(
+    name="medical-chat-bot",
+    version ="0.1.0",
+    author="Ashmit K C",
+    author_email="kcashmit0@gmail.com",
+    packages=find_packages(),
+    install_requires=[])
+
+
+    
